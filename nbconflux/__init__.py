2568a8ce4b30de783d49b19a6ed9c6e8d00cae72
Carol Willing <carolcode@willingconsulting.com>
2018-02-26 15:53:03 -0800

    Update __init__.py
    Correct the comment typo.
    
    @parente I was checking out the code, and I think there is a word missing here.

--- nbconflux/__init__.py
+++ nbconflux/__init__.py
@@ -1,7 +1,7 @@
 from .api import notebook_to_page
 
 # These three lines comes from versioneer.
-# Add to this file if you want, but do remove these.
+# Add to this file if you want, but do not remove these.
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
